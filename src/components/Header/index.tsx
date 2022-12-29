eb9e7f446fc15d4703af920915ae8ec4a7ce6a4a
tikagoncharov <tikagoncharov@gmail.com>
2022-12-29 20:33:53 +0700

    feat: add header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,8 +3,8 @@ import styles from './Header.module.scss'
 function Header(props: any) {
 	return (
 		<header className={styles.header}>
-			<h1 className="">HipHopTickTock</h1>
-			<img width={46} height={32} src="/img/burger-menu.svg" alt="" />
+			<h1 className={styles.appName}>HipHopTickTock</h1>
+			<img className={styles.burgerMenu} width={46} height={32} src="/img/burger-menu.svg" alt="" />
 		</header>
 	);
 }

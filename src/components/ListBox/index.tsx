8c18c5454ecdb4f74f259ba7b5e1cd03855658d6
tikagoncharov <tikagoncharov@gmail.com>
2023-01-10 20:14:21 +0700

    refactor: logic of changing arrow img styles

--- src/components/ListBox/index.tsx
+++ src/components/ListBox/index.tsx
@@ -6,9 +6,15 @@ const ListBox = (props: any) => {
 
 	return (
 		<details className={styles['list-box']} >
-			<summary className={styles.summary} onClick={() => setListStatus(!isListOpen)}>
+			<summary
+				className={styles.summary}
+				onClick={() => setListStatus(!isListOpen)}
+			>
 				<h2 className={styles['list-title']}>{props.title}</h2>
-				<img className={`${isListOpen ? styles['list-arrow'] : ''}`} style={{transition: '0.3s'}} src='/img/arrow-down.svg' alt="" />
+				<img
+					className={`${styles['list-arrow']} ${isListOpen ? styles['list-arrow_open'] : ''}`}
+					src='/img/arrow.svg' alt=""
+				/>
 			</summary>
 
 			<div className={styles['btns-container']}>{props.buttons}</div>

abfdf190dd7ca8cfee825b7af754bb02dd2d5d08
tikagoncharov <tikagoncharov@gmail.com>
2023-01-10 18:40:11 +0700

    feat: rotate list-arrow

--- src/components/ListBox/index.tsx
+++ src/components/ListBox/index.tsx
@@ -2,19 +2,13 @@ import styles from './ListBox.module.scss'
 import { useState } from "react";
 
 const ListBox = (props: any) => {
-	const [isOpen, setStatus] = useState(false);
-
-	let src = '/img/arrow-down.svg';
-
-	if (isOpen) {
-		src = '/img/arrow-up.svg'
-	}
+	const [isListOpen, setListStatus] = useState(false);
 
 	return (
 		<details className={styles['list-box']} >
-			<summary className={styles.summary} onClick={() => setStatus(!isOpen)}>
+			<summary className={styles.summary} onClick={() => setListStatus(!isListOpen)}>
 				<h2 className={styles['list-title']}>{props.title}</h2>
-				<img src={src} alt="" />
+				<img className={`${isListOpen ? styles['list-arrow'] : ''}`} style={{transition: '0.3s'}} src='/img/arrow-down.svg' alt="" />
 			</summary>
 
 			<div className={styles['btns-container']}>{props.buttons}</div>
